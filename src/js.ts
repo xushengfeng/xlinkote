f7c647833e2d0c3bcbe2bad34a986f37fa209e7c
xushengfeng <xushengfeng_zg@163.com>
2022-06-19 16:25:41 +0800

    画布 自由拖动

--- src/js.ts
+++ src/js.ts
@@ -1,12 +1,15 @@
 var main = document.getElementById("main");
-var o_e;
-var o_rect;
+
+var o_e: MouseEvent;
+var o_rect: DOMRect;
+
 document.onmousedown = (e) => {
     if (e.target == main) {
         o_e = e;
         o_rect = main.getBoundingClientRect();
     }
 };
+
 document.onmousemove = (e) => {
     mouse(e);
 };
@@ -14,9 +17,10 @@ document.onmouseup = (e) => {
     mouse(e);
     o_e = null;
 };
-var mouse = (e) => {
+var mouse = (e: MouseEvent) => {
     if (o_e) {
-        let x = o_rect.x + (e.clientX - o_e.clientX), y = o_rect.y + (e.clientY - o_e.clientY);
+        let x = o_rect.x + (e.clientX - o_e.clientX),
+            y = o_rect.y + (e.clientY - o_e.clientY);
         main.style.left = x + "px";
         main.style.top = y + "px";
     }
