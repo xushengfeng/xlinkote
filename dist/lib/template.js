66588a9a22c6f890160588232504920c6445500a
xushengfeng <xushengfeng_zg@163.com>
2022-06-21 21:14:03 +0800

    代码 编译到dist

--- dist/lib/template.js
+++ dist/lib/template.js
@@ -3,32 +3,29 @@ class x extends HTMLElement {
     constructor() {
         super();
     }
-
     connectedCallback() {
         var bar = document.createElement("div");
         bar.id = "x-x_bar";
         var size = document.createElement("input");
         var tag_type = document.createElement("input");
         var d = document.createElement("div");
-
         bar.append(size);
         bar.append(tag_type);
         bar.append(d);
         this.append(bar);
-
         var o_e;
         var o_rect;
-
         this.addEventListener("mousedown", (e) => {
-            if (e.button != 2) return;
+            if (e.button != 2)
+                return;
             o_e = e;
             o_rect = { x: this.offsetLeft, y: this.offsetTop };
             document.getElementById("画布").style.cursor = "move";
         });
-
         document.addEventListener("mousemove", (e) => {
             mouse(e);
-            if (o_e) move = true;
+            if (o_e)
+                move = true;
         });
         document.addEventListener("mouseup", (e) => {
             mouse(e);
@@ -38,19 +35,16 @@ class x extends HTMLElement {
         });
         var mouse = (e) => {
             if (o_e) {
-                let x = o_rect.x + (e.clientX - o_e.clientX),
-                    y = o_rect.y + (e.clientY - o_e.clientY);
+                let x = o_rect.x + (e.clientX - o_e.clientX), y = o_rect.y + (e.clientY - o_e.clientY);
                 this.style.left = x + "px";
                 this.style.top = y + "px";
             }
         };
-
         size.oninput = () => {
             let l = size.value.split(",");
             this.style.width = l[0];
             this.style.height = l[1];
         };
-
         tag_type.setAttribute("list", "tags");
         tag_type.onchange = () => {
             if (this.childNodes[1]) {
@@ -62,11 +56,9 @@ class x extends HTMLElement {
                 tag_type.blur();
             }
         };
-
         d.onclick = () => {
             this.remove();
         };
     }
 }
-
 window.customElements.define("x-x", x);
