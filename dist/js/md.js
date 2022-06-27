be07640bd51bcddb53da23d50fdc8049f1106de7
xushengfeng <xushengfeng_zg@163.com>
2022-06-27 20:41:56 +0800

    md +++details

--- dist/js/md.js
+++ dist/js/md.js
@@ -4,7 +4,24 @@ var md = window
     linkify: true,
     typographer: true,
 })
-    .use(window.markdownitTaskLists);
+    .use(window.markdownitTaskLists)
+    .use(window.markdownitContainer, "spoiler", {
+    validate: function (params) {
+        return params.trim().match(/^(.*)$/);
+    },
+    render: function (tokens, idx) {
+        var m = tokens[idx].info.trim().match(/^(.*)$/);
+        if (tokens[idx].nesting === 1) {
+            // opening tag
+            return "<details><summary>" + md.utils.escapeHtml(m[1]) + "</summary>\n";
+        }
+        else {
+            // closing tag
+            return "</details>\n";
+        }
+    },
+    marker: "+",
+});
 var defaultRender = md.renderer.rules.heading_open ||
     function (tokens, idx, options, env, self) {
         return self.renderToken(tokens, idx, options);
--- lib/self.d.ts
+++ lib/self.d.ts
@@ -4,4 +4,5 @@ declare interface Window {
     MathJax: any;
     markdownitTaskLists: any;
     JXG: any;
+    markdownitContainer: any;
 }
