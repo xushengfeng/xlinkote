e279410f8ddb0c08f330e2cd9db92f7a284ba1ac
xushengfeng <xushengfeng_zg@163.com>
2022-07-02 16:07:41 +0800

    md 公式行渲染

--- src/js/md.ts
+++ src/js/md.ts
@@ -36,3 +36,64 @@ md.renderer.rules.heading_open = function (tokens, idx, options, env, self) {
     }
     return defaultRender(tokens, idx, options, env, self);
 };
+
+md.renderer.rules["mathjax_inline"] = (tokens, idx, options, env, self) => self.renderToken(tokens, idx, options);
+md.renderer.rules.mathjax_inline = (tokens, idx, options, env, self) => {
+    return window.MathJax.tex2svg(tokens[idx].content).outerHTML.replace(`display="true"`, "");
+};
+md.inline.ruler.before("emphasis", "mathjax_inline", function (state, silent) {
+    var found,
+        content,
+        token,
+        max = state.posMax,
+        start = state.pos;
+
+    if (state.src.charCodeAt(start) !== 36 /* $ */) {
+        return false;
+    }
+    if (silent) {
+        return false;
+    } // don't run any pairs in validation mode
+    if (start + 2 >= max) {
+        return false;
+    }
+
+    state.pos = start + 1;
+
+    while (state.pos < max) {
+        if (state.src.charCodeAt(state.pos) === 36 /* $ */) {
+            found = true;
+            break;
+        }
+
+        state.md.inline.skipToken(state);
+    }
+
+    if (!found || start + 1 === state.pos) {
+        state.pos = start;
+        return false;
+    }
+
+    content = state.src.slice(start + 1, state.pos);
+
+    // don't allow unescaped spaces/newlines inside
+    if (content.match(/(^|[^\\])(\\\\)*\s/)) {
+        state.pos = start;
+        return false;
+    }
+
+    // found!
+    state.posMax = state.pos;
+    state.pos = start + 1;
+
+    token = state.push("mathjax_open", "span", 1);
+    token.markup = "$";
+    token = state.push("mathjax_inline", "", 0);
+    token.content = content;
+    token = state.push("mathjax_close", "span", -1);
+    token.markup = "$";
+
+    state.pos = state.posMax + 1;
+    state.posMax = max;
+    return true;
+});
