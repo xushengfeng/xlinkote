1ba46427d767339a036a37e487186e00bcd82aa5
xushengfeng <xushengfeng_zg@163.com>
2022-09-17 12:46:55 +0800

    画板 提取为颜色组件

--- src/js/js.ts
+++ src/js/js.ts
@@ -3283,86 +3283,104 @@ window.customElements.define("x-draw", draw);
 
 // 色彩选择器
 import color from "color";
-color_bar();
-var color_e: { o: { x: number; y: number }; c: { x: number; y: number }; t: HTMLElement } = null;
-function color_bar() {
-    const hsva = { h: 0, s: 0, l: 0, a: 0 };
 
-    const pel = document.getElementById("penc");
-    const broad = document.createElement("div");
-    const bg0 = document.createElement("div");
-    const pb = document.createElement("div");
-    const range = document.createElement("div");
-    const pr = document.createElement("div");
-    const c0 = document.createElement("div");
+class xcolor extends HTMLElement {
+    constructor() {
+        super();
+    }
 
-    pel.classList.add("color");
+    c = "#000000";
 
-    broad.classList.add("broad");
-    range.classList.add("range");
+    connectedCallback() {
+        let color_e: { o: { x: number; y: number }; c: { x: number; y: number }; t: HTMLElement } = null;
+        const hsva = { h: 0, s: 0, l: 0, a: 0 };
 
-    broad.append(bg0);
-    bg0.style.background = `linear-gradient(rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0) 100%), linear-gradient(to right, rgb(255, 255, 255) 0%, hsl(0,100%,50%) 100%)`;
-    pel.append(broad);
+        const pel = this;
+        const broad = document.createElement("div");
+        const bg0 = document.createElement("div");
+        const pb = document.createElement("div");
+        const range = document.createElement("div");
+        const pr = document.createElement("div");
+        const c0 = document.createElement("div");
 
-    pb.classList.add("color_pointer");
-    bg0.append(pb);
+        pel.classList.add("color");
 
-    range.style.background = `linear-gradient(to right, red 0%, #ff0 17%, lime 33%, cyan 50%, blue 66%, #f0f 83%, red 100%)`;
-    range.style.height = "20px";
-    pel.append(range);
+        broad.classList.add("broad");
+        range.classList.add("range");
 
-    pr.classList.add("color_range_p");
-    range.append(pr);
+        broad.append(bg0);
+        bg0.style.background = `linear-gradient(rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0) 100%), linear-gradient(to right, rgb(255, 255, 255) 0%, hsl(0,100%,50%) 100%)`;
+        pel.append(broad);
 
-    pel.append(c0);
-    c0.style.width = "20px";
-    c0.style.height = "20px";
+        pb.classList.add("color_pointer");
+        bg0.append(pb);
 
-    range.onpointerdown = (e) => {
-        color_e = { o: { x: e.offsetX, y: e.offsetY }, c: { x: e.clientX, y: e.clientY }, t: range };
-    };
-    bg0.onpointerdown = (e) => {
-        color_e = { o: { x: e.offsetX, y: e.offsetY }, c: { x: e.clientX, y: e.clientY }, t: bg0 };
-        console.log(color_e);
-    };
-    const f = (e: PointerEvent) => {
-        if (color_e.t == range) {
-            let x = (e.clientX - color_e.c.x + color_e.o.x) / range.offsetWidth;
-            if (x < 0) x = 0;
-            if (x > 1) x = 1;
-            pr.style.left = x * range.offsetWidth + "px";
-            let h = Math.round(360 * x);
-            if (h < 0) h = 0;
-            if (h >= 360) h = 0;
-            console.log(h);
-            hsva.h = h;
-            bg0.style.background = `linear-gradient(rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0) 100%), linear-gradient(to right, rgb(255, 255, 255) 0%, hsl(${h},100%,50%) 100%)`;
-        } else if (color_e.t == bg0) {
-            let x = (e.clientX - color_e.c.x + color_e.o.x) / bg0.offsetWidth;
-            let y = (e.clientY - color_e.c.y + color_e.o.y) / bg0.offsetHeight;
-            console.log(x, y);
-            if (x < 0) x = 0;
-            if (x > 1) x = 1;
-            if (y < 0) y = 0;
-            if (y > 1) y = 1;
-            pb.style.left = x * bg0.offsetWidth + "px";
-            pb.style.top = y * bg0.offsetHeight + "px";
-            hsva.s = x * 100;
-            hsva.l = (1 - y) * 100;
-        }
-        let hsv = color.hsv(hsva.h, hsva.s, hsva.l);
-        c0.style.backgroundColor = pb.style.backgroundColor = `${hsv.rgb().string()}`;
-    };
-    document.addEventListener("pointermove", (e) => {
-        if (!color_e) return;
-        f(e);
-    });
-    document.addEventListener("pointerup", (e) => {
-        f(e);
-        color_e = null;
-    });
+        range.style.background = `linear-gradient(to right, red 0%, #ff0 17%, lime 33%, cyan 50%, blue 66%, #f0f 83%, red 100%)`;
+        range.style.height = "20px";
+        pel.append(range);
+
+        pr.classList.add("color_range_p");
+        range.append(pr);
+
+        pel.append(c0);
+        c0.style.width = "20px";
+        c0.style.height = "20px";
+
+        range.onpointerdown = (e) => {
+            color_e = { o: { x: e.offsetX, y: e.offsetY }, c: { x: e.clientX, y: e.clientY }, t: range };
+        };
+        bg0.onpointerdown = (e) => {
+            color_e = { o: { x: e.offsetX, y: e.offsetY }, c: { x: e.clientX, y: e.clientY }, t: bg0 };
+            console.log(color_e);
+        };
+        const f = (e: PointerEvent) => {
+            if (color_e.t == range) {
+                let x = (e.clientX - color_e.c.x + color_e.o.x) / range.offsetWidth;
+                if (x < 0) x = 0;
+                if (x > 1) x = 1;
+                pr.style.left = x * range.offsetWidth + "px";
+                let h = Math.round(360 * x);
+                if (h < 0) h = 0;
+                if (h >= 360) h = 0;
+                console.log(h);
+                hsva.h = h;
+                bg0.style.background = `linear-gradient(rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0) 100%), linear-gradient(to right, rgb(255, 255, 255) 0%, hsl(${h},100%,50%) 100%)`;
+            } else if (color_e.t == bg0) {
+                let x = (e.clientX - color_e.c.x + color_e.o.x) / bg0.offsetWidth;
+                let y = (e.clientY - color_e.c.y + color_e.o.y) / bg0.offsetHeight;
+                console.log(x, y);
+                if (x < 0) x = 0;
+                if (x > 1) x = 1;
+                if (y < 0) y = 0;
+                if (y > 1) y = 1;
+                pb.style.left = x * bg0.offsetWidth + "px";
+                pb.style.top = y * bg0.offsetHeight + "px";
+                hsva.s = x * 100;
+                hsva.l = (1 - y) * 100;
+            }
+            let hsv = color.hsv(hsva.h, hsva.s, hsva.l);
+            c0.style.backgroundColor = pb.style.backgroundColor = `${hsv.rgb().string()}`;
+            this.c = hsv.hexa();
+        };
+        document.addEventListener("pointermove", (e) => {
+            if (!color_e) return;
+            f(e);
+        });
+        document.addEventListener("pointerup", (e) => {
+            f(e);
+            color_e = null;
+        });
+    }
+
+    get value() {
+        return this.c;
+    }
+    set value(s) {
+        this.c = s;
+    }
 }
+
+window.customElements.define("x-color", xcolor);
 
 const link_bar = document.getElementById("link_bar");
 const link_ids = document.getElementById("link_ids");
