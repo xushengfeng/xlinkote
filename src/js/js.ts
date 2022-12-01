a898d5076198f51a329f68f69af9f17f122f4139
xushengfeng <xushengfeng_zg@163.com>
2022-12-02 07:44:50 +0800

    版本 0.11.5

--- src/js/js.ts
+++ src/js/js.ts
@@ -1457,6 +1457,7 @@ function version_tr(obj): 集type {
         case "0.11.2":
         case "0.11.3":
         case "0.11.4":
+        case "0.11.5":
             return obj;
         default:
             put_toast(`文件版本是 ${v}，与当前软件版本 ${packagejson.version} 不兼容，请升级软件`);
