cf2f9bff2a04b70fa8b400ec7ca2fac0bd70cf13
xushengfeng <xushengfeng_zg@163.com>
2022-12-05 15:30:45 +0800

    主要 模式切换快捷键

--- src/js/js.ts
+++ src/js/js.ts
@@ -1225,6 +1225,19 @@ document.onkeydown = (e) => {
                 e.preventDefault();
                 undo(false);
             }
+            break;
+        case "i":
+            if (模式 != "浏览") {
+                set_模式("浏览");
+                e.preventDefault();
+                if (z.聚焦元素.querySelector("x-md")) (z.聚焦元素.querySelector("x-md") as markdown).edit = true;
+            }
+            break;
+        case "Escape":
+            if (模式 == "浏览") {
+                set_模式("设计");
+            }
+            break;
     }
 };
 
