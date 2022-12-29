368b4f06a8c85cef693d7d75a7bd9861b48a41cf
xushengfeng <xushengfeng_zg@163.com>
2022-12-29 17:07:50 +0800

    主要 排除链接非法值

--- src/js/js.ts
+++ src/js/js.ts
@@ -1736,6 +1736,9 @@ function version_tr(obj): 集type {
             obj.meta.version = "0.17.2";
         case "0.17.2":
         case "0.17.3":
+            delete obj.链接[""];
+            delete obj.链接["0"][""];
+            delete obj.链接["0"]["0"];
             return obj;
         default:
             put_toast(`文件版本是 ${v}，与当前软件版本 ${packagejson.version} 不兼容，请升级软件`);
