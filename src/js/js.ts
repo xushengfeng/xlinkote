7a23c6780616df8501b16c49b7ad23e791cc05a0
xushengfeng <xushengfeng_zg@163.com>
2023-01-18 21:33:13 +0800

    版本 0.21.0

--- src/js/js.ts
+++ src/js/js.ts
@@ -2185,6 +2185,7 @@ function version_tr(obj): 集type {
         case "0.20.1":
         case "0.20.2":
         case "0.20.3":
+        case "0.21.0":
             return obj;
         default:
             put_toast(`文件版本是 ${v}，与当前软件版本 ${packagejson.version} 不兼容，请升级软件`);
