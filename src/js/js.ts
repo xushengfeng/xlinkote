9b995f202313cfce31ba20f10c15cb54167ce3a4
xushengfeng <xushengfeng_zg@163.com>
2023-01-18 17:38:35 +0800

    箭头链接 点击中间不创建链接

--- src/js/js.ts
+++ src/js/js.ts
@@ -1491,7 +1491,7 @@ document.addEventListener("pointerup", (e: PointerEvent) => {
         data_changed();
     }
     if (free_drag || free_old_point) z.reflash();
-    if (!free_drag && !free_move && free_old_point) {
+    if (!free_drag && !free_move && free_old_point && free_o_a != -1) {
         if (!free_link) {
             let id = uuid_id();
             free_link = id;
