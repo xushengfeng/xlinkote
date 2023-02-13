6b2708162e4626c4f557d4e08e71087e51b09ea8
xushengfeng <xushengfeng_zg@163.com>
2023-02-13 20:33:15 +0800

    文件 链接添加衰减系数

--- src/js/js.ts
+++ src/js/js.ts
@@ -2166,6 +2166,13 @@ function version_tr(obj): 集type {
             }
             obj.meta.version = "0.20.0";
         case version_in(v, "0.20.0", "0.21.8"):
+            for (let i in obj.链接) {
+                for (let j in obj.链接[i]) {
+                    obj.链接[i][j]["s"] = default_down_s;
+                }
+            }
+            obj.meta.version = "0.22.0";
+        case version_in(v, "0.22.0", "0.22.0"):
             return obj;
         default:
             put_toast(`文件版本是 ${v}，与当前软件版本 ${packagejson.version} 不兼容，请升级软件`);
